ac190b58d6ba2af0701f4d7304cb8312cbdb2957
BhamareHarshali <harshalibhamare729@gmail.com>
2025-07-31 15:26:55 +0530

    Delete Assignments/Assignment no.1 directory

--- Assignments/Assignment no.1/problem2.py
+++
@@ -1,9 +0,0 @@
-#2 .Write a program to calculate area of rectangle based on length and breadth.
-
-length = int(input("Enter the length of ractangle : "))
-breadth = int(input("Enter the breadth of ractangle : "))
-
-area = length * breadth  #l*b 
-
-#Display output 
-print(f'area of {length} and {breadth} is {area}')
